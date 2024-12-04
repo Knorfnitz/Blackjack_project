2156779ed7aacccdf3068c3a56f1419872196421
Antonino taormina <taormina85toni@gmail.com>
2024-12-04 17:38:22 +0100

    Lezter Push

--- BlackJack/Views/HomeView/HomeView.swift
+++ BlackJack/Views/HomeView/HomeView.swift
@@ -1,117 +1,123 @@
-//
-//  HomeView.swift
-//  BlackJack
-//
-//  Created by Alexander Hasecke on 03.12.24.
-//  04.12.2024 Bearbeitet Von @Tonytaor
-
 import SwiftUI
 
-
-    //var player1: Player = Player(name: "Player1", gameHistory: [], coins: 1000, email: "player1@example.com", password: "Player1")
-
-    struct HomeView: View {
-        @StateObject var playerViewVM: PlayerViewVM
-        var body: some View {
-            NavigationView {
-                ZStack {
-                    Image("GameTable")
+struct HomeView: View {
+    @StateObject var playerViewVM: PlayerViewVM
+    @State private var activeView: String? = nil
+    
+    var body: some View {
+        ZStack {
+            Image("GameTable")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                // Top-Bar mit Spielername und Coins
+                HStack {
+                    Text(playerViewVM.player.name)
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .foregroundColor(BlackjackTheme.textColor)
+                        .padding(.leading, 30)
+                    
+                    Spacer()
+                    
+                    Text("\(playerViewVM.player.coins)")
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .foregroundColor(BlackjackTheme.textColor)
+                        .padding(.trailing, 50)
+                }
+                .padding(.top,60)
+                .ignoresSafeArea()
+                Spacer()
+            }
+            
+            VStack(spacing: 20) {
+                Text("Willkommen bei \nBlackjack!")
+                    .font(.system(size: 55, weight: .bold, design: .rounded))
+                    .foregroundColor(BlackjackTheme.highlightColor1)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+                
+                HStack {
+                    Image("Chip")
                         .resizable()
-                        .scaledToFill()
-                        .ignoresSafeArea()
+                        .scaledToFit()
+                        .frame(width: 50)
+                        .padding(50)
                     
-                    //@TONI: Top-Bar mit Spielername und Coins
-                    
-                    VStack {
-                        HStack {
-                            Text(playerViewVM.player.name)
-                                .font(.system(size: 20, weight: .bold, design: .rounded))
-                                .foregroundColor(BlackjackTheme.textColor)
-                                .padding(.leading, 50)//@TONI:Nach RX verschieben
-                            
-                            Spacer()
-                            
-                            Text("\(playerViewVM.player.coins)")
-                                .font(.system(size: 20, weight: .bold, design: .rounded))
-                                .foregroundColor(BlackjackTheme.textColor)
-                                .padding(.trailing, 50)//@TONI:Nach LX verschieben
-                        }
-                        .padding(.top, 20)
-                        .ignoresSafeArea()
-                        Spacer()
+                    Image("Chip")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50)
+                        .padding(55)
+                }
+                
+                // Buttons für Navigation
+                VStack(spacing: 20) {
+                    // Spiel starten - activeView ändern
+                    Button(action: {
+                        activeView = "Game"
+                    }) {
+                        Text("Spiel starten")
+                            .font(.title2.bold())
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(BlackjackTheme.highlightColor1)
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                            .shadow(radius: 8)
                     }
                     
-                    VStack(spacing: 20) {
-                                        
-                                        Text("Willkommen bei \nBlackjack!")
-                                            .font(.system(size: 55, weight: .bold, design: .rounded))
-                                            .foregroundColor(BlackjackTheme.highlightColor1)
-                                            .multilineTextAlignment(.center)
-                                            .padding(.bottom, 20)
-                                        
-                                        
-                                        HStack {
-                                            Image("Chip")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 50)
-                                                .padding(50)
-                                            
-                                            Image("Chip")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 50)
-                                                .padding(55)
-                                        }
-                                        //@TONI: Button
-                                        VStack(spacing: 20) {
-                                            NavigationLink(destination: GameView()) {
-                                                Text("Spiel starten")
-                                                    .font(.title2.bold())
-                                                    .padding()
-                                                    .frame(maxWidth: .infinity)
-                                                    .background(BlackjackTheme.highlightColor1)
-                                                    .foregroundColor(.white)
-                                                    .cornerRadius(12)
-                                                    .shadow(radius: 8)
-                                            }
-                                            
-                                            NavigationLink(destination: StatsView()) {
-                                                Text("Statistiken")
-                                                    .font(.title2.bold())
-                                                    .padding()
-                                                    .frame(maxWidth: .infinity)
-                                                    .background(BlackjackTheme.highlightColor1)
-                                                    .foregroundColor(.white)
-                                                    .cornerRadius(12)
-                                                    .shadow(radius: 8)
-                                            }
-                                            
-                                            NavigationLink(destination: SettingsView()) {
-                                                Text("Einstellungen")
-                                                    .font(.title2.bold())
-                                                    .padding()
-                                                    .frame(maxWidth: .infinity)
-                                                    .background(BlackjackTheme.highlightColor1)
-                                                    .foregroundColor(.white)
-                                                    .cornerRadius(12)
-                                                    .shadow(radius: 8)
-                                            }
-                                        }
-                                        .padding(.horizontal, 30)
-                                        .padding(.bottom, 40)
-                                    }
-                                    .padding()
-                                    .foregroundColor(BlackjackTheme.textColor)
-                                    .cornerRadius(20)
-                                    .shadow(radius: 20)
-                                }
-                                .navigationBarHidden(true)
-                            }
-                        }
+                    // Wechsel zu StatsView
+                    Button(action: {
+                        activeView = "Stats"
+                    }) {
+                        Text("Statistiken")
+                            .font(.title2.bold())
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(BlackjackTheme.highlightColor1)
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                            .shadow(radius: 8)
                     }
+                    
+                    // Wechsel zu SettingsView
+                    Button(action: {
+                        activeView = "Settings"
+                    }) {
+                        Text("Einstellungen")
+                            .font(.title2.bold())
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(BlackjackTheme.highlightColor1)
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                            .shadow(radius: 8)
+                    }
+                }
+                .padding(.horizontal, 30)
+                .padding(.bottom, 40)
+            }
+            .padding()
+            .foregroundColor(BlackjackTheme.textColor)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            
+            // Zeige die aktive Ansicht basierend auf activeView
+            if activeView == "Game" {
+                GameView()
+            } else if activeView == "Stats" {
+                StatsView(gameStats: GameStats())
+            } else if activeView == "Settings" {
+                
+            }
+        }
+        .navigationBarBackButtonHidden(true) // Der Back-Button wird ausgeblendet
+                .navigationBarTitle("Home", displayMode: .inline)
+    }
+}
 
-                    #Preview {
-                        HomeView(playerViewVM: PlayerViewVM())
-                    }
-
+#Preview {
+    HomeView(playerViewVM: PlayerViewVM())
+}
--- BlackJack/Views/StatsView/StatsView.swift
+++ BlackJack/Views/StatsView/StatsView.swift
@@ -5,18 +5,81 @@
 //  Created by Alexander Hasecke on 03.12.24.
 //
 
+
 import SwiftUI
 
 struct StatsView: View {
+    @ObservedObject var gameStats: GameStats
+    
     var body: some View {
-     //   Text("gespielte Spiele")
-        Text("Guthaben / nach Zeit")
-       // Text("Win Rate")
-        Text("gespielte Spiele")
-        Text("gewinn/verlust")
+        NavigationStack {  // Um die Navigation zu ermöglichen
+            ZStack {
+                ScreenBackgroundColor()
+                
+                VStack(spacing: 16) {
+                    Text("Statistiken")
+                        .font(.largeTitle.bold())
+                        .foregroundColor(.white)
+                        .padding(.bottom, 20)
+                    
+                    Text("Total Games: \(gameStats.totalGames)")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                    Text("Wins: \(gameStats.wins)")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                    Text("Losses: \(gameStats.losses)")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                    if gameStats.totalGames > 0 {
+                        Text("Win Percentage: \(gameStats.wins * 100 / gameStats.totalGames)%")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                    } else {
+                        Text("Win Percentage: N/A")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Text("Highest Score: \(gameStats.highestScore)")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                    Text("Average Score: \(gameStats.averageScore, specifier: "%.2f")")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                    
+                    Spacer()  // Damit der Button am unteren Rand erscheint
+                    
+                    // Hier verwenden wir einen NavigationLink, um den Benutzer zum HomeView zu navigieren
+                    NavigationLink(destination: HomeView(playerViewVM: PlayerViewVM())) {
+                        Text("Zurück zum Home")
+                            .font(.title2)
+                            .bold()
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.yellow.opacity(0.8))  // Hier kannst du eine benutzerdefinierte goldene Farbe verwenden
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding([.leading, .trailing], 20)
+                    }
+                }
+                .padding(.top, -55)
+                .multilineTextAlignment(.center)
+                .navigationBarTitle("Statistiken", displayMode: .inline)
+            }
+        }
     }
 }
 
-#Preview {
-    StatsView()
-}
+/**
+ ```
+ #Preview {
+     let totalGames: Int = 0
+     let wins: Int = 0
+     let losses: Int = 0
+     let highestScore: Int = 0
+     let averageScore: Double = 0.0
+     StatsView(gameStats: GameStats(totalGames: totalGames, wins: wins, losses: losses, highestScore: highestScore, averageScore: averageScore))
+ }
+ ```
+ **/
